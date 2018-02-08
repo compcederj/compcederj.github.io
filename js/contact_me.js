a5c863d0505eab8bc9351a053e2bb494884d7335
Thiago da Cunha Borges <thiagoborges@id.uff.br>
2018-02-08 17:38:48 -0200

    Alterações no formulário de contato

--- js/contact_me.js
+++ js/contact_me.js
@@ -1,11 +1,11 @@
-$(function() {
+$(function () {
 
   $("#contactForm input,#contactForm textarea").jqBootstrapValidation({
     preventSubmit: true,
-    submitError: function($form, event, errors) {
+    submitError: function ($form, event, errors) {
       // additional error messages or events
     },
-    submitSuccess: function($form, event) {
+    submitSuccess: function ($form, event) {
       event.preventDefault(); // prevent default submit behaviour
       // get values from FORM
       var name = $("input#name").val();
@@ -29,7 +29,7 @@ $(function() {
           message: message
         },
         cache: false,
-        success: function() {
+        success: function () {
           // Success message
           $('#success').html("<div class='alert alert-success'>");
           $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
@@ -41,35 +41,37 @@ $(function() {
           //clear all fields
           $('#contactForm').trigger("reset");
         },
-        error: function() {
+        error: function () {
           // Fail message
           $('#success').html("<div class='alert alert-danger'>");
           $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
-          $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
+          $('#success > .alert-danger').append($("<strong>").text("Desculpa, " + firstName + ", nosso servidor de email não está funcionando. Tente mais tarde, ou "));
+          $('#success > .alert-danger').append('<a href="http://saladeestudos.esy.es/#contact">').text("clique aqui!");
+          $('#success > .alert-danger').append('</a>');
           $('#success > .alert-danger').append('</div>');
           //clear all fields
           $('#contactForm').trigger("reset");
         },
-        complete: function() {
-          setTimeout(function() {
+        complete: function () {
+          setTimeout(function () {
             $this.prop("disabled", false); // Re-enable submit button when AJAX call is complete
           }, 1000);
         }
       });
     },
-    filter: function() {
+    filter: function () {
       return $(this).is(":visible");
     },
   });
 
-  $("a[data-toggle=\"tab\"]").click(function(e) {
+  $("a[data-toggle=\"tab\"]").click(function (e) {
     e.preventDefault();
     $(this).tab("show");
   });
 });
 
 /*When clicking on Full hide fail/success boxes */
-$('#name').focus(function() {
+$('#name').focus(function () {
   $('#success').html('');
 });
