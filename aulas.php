fc020017d6e05ca6760a40c1147fe79fffd72efb
Thiago da Cunha Borges <thiagoborges@id.uff.br>
2018-02-19 16:34:24 -0300

    Diagramação do rodapé

---
+++ aulas.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="pt_br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta name="keywords" content="thcborges, ">
+    <meta name="author" content="Thiago da Cunha Borges">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Title</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css"
+          integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+    <!-- <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon"> -->
+    <!-- <link rel="stylesheet" href=""> -->
+    <style>
+        html, body {
+            height: 100%;
+            width: 100%;
+        }
+        #aula {
+            height: 100%;
+            width: 100%;
+        }
+    </style>
+</head>
+<body>
+<iframe id="aula" src="http://videoaula.rnp.br/v.php?f=/cederj/sistemas_comp/ead05019/Aula_001/Aula_001.xml"
+        frameborder="0" scrolling="no"></iframe>
+<iframe scrolling="no" frameborder="0" src="https://coinpot.co/mine/bitcoincore/?ref=796EA4311353&mode=widget"
+        style="overflow:hidden;width:324px;height:470px;"></iframe>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+        crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"
+        integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4"
+        crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js"
+        integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1"
+        crossorigin="anonymous"></script>
+</body>
+</html>
